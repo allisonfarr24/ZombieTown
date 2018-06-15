1908e965a1b63938787bcfec719ba85d862c1c2a
Allison Farr <allisonfarr24@gmail.com>
2018-06-15 09:10:33 -0400

    initial commit

---
+++ src/main/java/com/zombieproject/ZombieTown/HomeController.java
@@ -0,0 +1,5 @@
+package com.zombieproject.ZombieTown;
+
+public class HomeController {
+
+}
---
+++ src/main/java/com/zombieproject/ZombieTown/ZombieTownApplication.java
@@ -0,0 +1,12 @@
+package com.zombieproject.ZombieTown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZombieTownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZombieTownApplication.class, args);
+	}
+}
