40d3993799d20b272a6db3a656114aea314ffeb1
reakoffa <reakoffa@gmail.com>
2018-06-15 09:55:49 -0400

    key working

--- src/main/java/com/zombieproject/ZombieTown/HomeController.java
+++ src/main/java/com/zombieproject/ZombieTown/HomeController.java
@@ -1,5 +1,20 @@
 package com.zombieproject.ZombieTown;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class HomeController {
 
+	@Value("${zombietown.apikey}")
+	private String key;
+	
+	
+		@RequestMapping("/")
+		public ModelAndView home() {
+			System.out.println(key);
+			return new ModelAndView("index");
+		}
 }
