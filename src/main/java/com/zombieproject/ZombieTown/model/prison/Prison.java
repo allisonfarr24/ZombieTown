5fda5a8b43c5beff6696b14b65e7dbfebb854f23
Allison Farr <allisonfarr24@gmail.com>
2018-06-18 13:43:23 -0400

    PrisonController created and pulls data to MySQL database

--- src/main/java/com/zombieproject/ZombieTown/model/prison/Prison.java
+++ src/main/java/com/zombieproject/ZombieTown/model/prison/Prison.java
@@ -1,29 +1,38 @@
-package com.zombieproject.ZombieTown.model;
+package com.zombieproject.ZombieTown.model.prison;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Table(name="prison")
 public class Prison {
-	
+
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name="id")
 	private int id;
+	@Column(name="code")
+	private String code;
+	@Column(name="latitude")
 	private String latitude;
+	@Column(name="longitude")
 	private String longitude;
-	
+
 	public Prison() {
-		
+
 	}
 
-	public Prison(int id, String latitude, String longitude) {
+	public Prison(int id, String code, String latitude, String longitude) {
 		super();
 		this.id = id;
+		this.code = code;
 		this.latitude = latitude;
 		this.longitude = longitude;
 	}
@@ -52,11 +61,17 @@ public class Prison {
 		this.longitude = longitude;
 	}
 
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
 	@Override
 	public String toString() {
-		return "Prison [id=" + id + ", latitude=" + latitude + ", longitude=" + longitude + "]";
+		return "Prison [id=" + id + ", code=" + code + ", latitude=" + latitude + ", longitude=" + longitude + "]";
 	}
-	
-	
 
 }
--- src/main/java/com/zombieproject/ZombieTown/repository/PrisonRepository.java
+++ src/main/java/com/zombieproject/ZombieTown/repository/PrisonRepository.java
@@ -2,12 +2,12 @@ package com.zombieproject.ZombieTown.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.zombieproject.ZombieTown.model.Prison;
+import com.zombieproject.ZombieTown.model.prison.Prison;
 
 public interface PrisonRepository extends JpaRepository<Prison, Integer> {
-	
 
-	
-	
 
+	 
+	
+	
 }
