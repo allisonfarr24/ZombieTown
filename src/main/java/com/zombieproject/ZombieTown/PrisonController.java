9b4c351a7b00844ba2fe3274aaa45474dd8bed3c
Allison Farr <allisonfarr24@gmail.com>
2018-06-19 11:31:37 -0400

    changes to prison controller

--- src/main/java/com/zombieproject/ZombieTown/PrisonController.java
+++ src/main/java/com/zombieproject/ZombieTown/PrisonController.java
@@ -4,15 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-import org.springframework.web.client.RestTemplate;
-
-import org.springframework.web.bind.annotation.RequestParam;
-
-import org.springframework.web.servlet.ModelAndView;
-
-import com.zombieproject.ZombieTown.model.prison.JsonPrisonResponse;
 import com.zombieproject.ZombieTown.model.prison.Prison;
 import com.zombieproject.ZombieTown.repository.PrisonRepository;
 
@@ -24,26 +16,6 @@ public class PrisonController {
 
 	// This method only needs to be run once to populate database in MySQL
 
-	 @RequestMapping("/prisontest")
-	 public ModelAndView prison() {
-	 ModelAndView mv = new ModelAndView("index");
-	 RestTemplate restTemplate = new RestTemplate();
-	 JsonPrisonResponse response = restTemplate.getForObject(
-	 "https://www.bop.gov/PublicInfo/execute/locations/?todo=query&output=json",
-	 JsonPrisonResponse.class);
-	
-	 Prison[] list = response.getLocations();
-	 System.out.println(list.toString());
-	 for (Prison prison : list) {
-	 p.save(prison);
-	 }
-	
-	 return mv;
-	 }
-
-
-
-	
 //	 @RequestMapping("/prisontest")
 //	 public ModelAndView prison() {
 //	 ModelAndView mv = new ModelAndView("index");
@@ -61,11 +33,7 @@ public class PrisonController {
 //	 return mv;
 //	 }
 	
-	@RequestMapping("/counter")
-	public ModelAndView prison(@RequestParam double lat, @RequestParam double lng) {
-		int count = prisonCount(lat, lng);
-		return new ModelAndView("counter", "num", count);
-	}
+	
 
 	public double prisonDistance(String latitude, String longitude, double lat, double lng) {
 
